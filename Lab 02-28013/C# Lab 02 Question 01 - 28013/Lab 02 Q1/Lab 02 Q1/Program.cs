3d15ccff02d5d65d300610bf563d401056c8b96f
dmsdiwyanjali <140637069+dmsdiwyanjali@users.noreply.github.com>
2023-07-31 11:21:58 +0530

    Add files via upload

---
+++ Lab 02-28013/C# Lab 02 Question 01 - 28013/Lab 02 Q1/Lab 02 Q1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_02_Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1,num2,sum;
+
+            Console.WriteLine("Enter 1st number: ");
+            num1=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter 2nd number: ");
+            num2=int.Parse(Console.ReadLine()); 
+
+            sum=num1 + num2;
+            Console.WriteLine("The sum of 1st and 2nd numbers is: " +sum);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
